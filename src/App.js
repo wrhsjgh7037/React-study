089a1c3c6df53121dff0eba4768e5d43bc56aa5f
whrjsgh7037dsf <ckh7037@gmail.com>
2020-10-07 14:35:25 +0900

    Create React Project

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import "./App.css";
 import Paper from "@material-ui/core/Paper";
 import Table from "@material-ui/core/Table";
 import TableHead from "@material-ui/core/TableHead";
-import TableBody from "@material-ui/core/TableBody";
+import Tablebody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import TableBody from "@material-ui/core/TableBody";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
   root: {
     width: "100%",
     marginTop: theme.spacing.unit * 3,
-    overflowX: "auto",
+    overflowx: "auto",
   },
   table: {
     minWidth: 1080,
@@ -23,30 +24,31 @@ const styles = (theme) => ({
 const customers = [
   {
     id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "조건호",
-    birthday: "111111",
+    imge: "http://placeimg.com/64/64/1",
+    name: "홍길동",
+    birthday: "961222",
     gender: "남자",
-    job: "학생",
+    job: "대학생",
   },
+
   {
     id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "홍길동",
-    birthday: "222222",
+    imge: "http://placeimg.com/64/64/2",
+    name: "조건호",
+    birthday: "020724",
     gender: "남자",
-    job: "도적",
+    job: "고등학생",
   },
+
   {
     id: 3,
-    image: "https://placeimg.com/64/64/3",
+    imge: "http://placeimg.com/64/64/3",
     name: "김영훈",
-    birthday: "333333",
+    birthday: "020910",
     gender: "남자",
     job: "회사원",
   },
 ];
-
 class App extends Component {
   render() {
     const { classes } = this.props;
@@ -60,17 +62,16 @@ class App extends Component {
               <TableCell>이름</TableCell>
               <TableCell>생년월일</TableCell>
               <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
+              <TableCell>작업</TableCell>
             </TableRow>
           </TableHead>
-
           <TableBody>
             {customers.map((c) => {
               return (
                 <Customer
                   key={c.id}
                   id={c.id}
-                  image={c.image}
+                  imge={c.imge}
                   name={c.name}
                   birthday={c.birthday}
                   gender={c.gender}
@@ -84,4 +85,5 @@ class App extends Component {
     );
   }
 }
+
 export default withStyles(styles)(App);
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -5,18 +5,16 @@ import TableCell from "@material-ui/core/TableCell";
 class Customer extends React.Component {
   render() {
     return (
-      <div>
-        <TableRow>
-          <TableCell>{this.props.id}</TableCell>
-          <TableCell>
-            <img src={this.props.image} alt="profile" />
-          </TableCell>
-          <TableCell>{this.props.name}</TableCell>
-          <TableCell>{this.props.birthday}</TableCell>
-          <TableCell>{this.props.gender}</TableCell>
-          <TableCell>{this.props.job}</TableCell>
-        </TableRow>
-      </div>
+      <TableRow>
+        <TableCell>{this.props.id}</TableCell>
+        <TableCell>
+          <img src={this.props.imge} alt="profile" />
+        </TableCell>
+        <TableCell>{this.props.name}</TableCell>
+        <TableCell>{this.props.birthday}</TableCell>
+        <TableCell>{this.props.gender}</TableCell>
+        <TableCell>{this.props.job}</TableCell>
+      </TableRow>
     );
   }
 }
